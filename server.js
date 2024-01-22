390c87fdf62c08d7bffde1acd2c01062f0ba63f0
Mahussi DATONGNON <84915773+mahussidatongnon@users.noreply.github.com>
2024-01-22 21:43:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
-// Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static( path.dirname + './dist/assignment-app'));
+app.use(express.static(path.join(__dirname, './dist/assignment-app')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/dist/assignment-app/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/assignment-app/index.html'));
+});
+
+// Ajouter le type MIME approprié pour les fichiers JavaScript
+app.get('*.js', (req, res, next) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  next();
 });
 
 // Start the app by listening on the default port
-app.listen(process.env.PORT || 8081);
+app.listen(process.env.PORT || 8081, () => {
+  console.log(`Server is running on http://localhost:${process.env.PORT || 8081}`);
+});
